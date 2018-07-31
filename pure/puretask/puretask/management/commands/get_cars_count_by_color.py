cd5cd4766ed3f7b175e58edd39c3ae9c2bcaf469
Alexey Shrub <ashrub@yandex.ru>
2018-07-31 15:53:57 +0300

    ORM request

---
+++ pure/puretask/puretask/management/commands/get_cars_count_by_color.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Count
+from django.contrib.postgres.fields.jsonb import KeyTextTransform
+from puretask.models import Car
+
+class Command(BaseCommand):
+    help = 'Group by and count cars by color'
+
+    def handle(self, *args, **options):
+        color_id = KeyTextTransform('colorId', 'parameters')
+        cars = Car.objects.annotate(color_id = color_id).values('color_id').annotate(Count('id')).order_by()
+        for car in cars:
+            self.stdout.write(self.style.SUCCESS(repr(car)))
