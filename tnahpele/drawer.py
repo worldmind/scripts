36933f2070169a86ed168d44def82fd5277f1822
Alexey Shrub <ashrub@yandex.ru>
2018-08-21 22:14:03 +0300

    Шлифовка

--- tnahpele/drawer.py
+++ tnahpele/drawer.py
@@ -8,19 +8,36 @@ Draw pseudographics by commands from file.
 import re
 from docopt import docopt
 
-CANVAS_VERTICAL_BORDER = '|'
-CANVAS_HORIZONTAL_BORDER = '-'
-LINE_DOT = 'x'
+# Type Point is not bad idea, but I am not sure that it make code more
+# readable here because we do a lot of job with coordinates, not with points
 
-CANVAS_RE = re.compile(r'^ C \s+ (?P<width>\d+) \s+ (?P<height>\d+) \s*$',  re.VERBOSE)
-LINE_RE = re.compile(r'^ L \s+ (?P<x1>\d+) \s+ (?P<y1>\d+) \s+ (?P<x2>\d+) \s+ (?P<y2>\d+) \s*$',  re.VERBOSE)
-RECTANGLE_RE = re.compile(r'^ R \s+ (?P<x1>\d+) \s+ (?P<y1>\d+) \s+ (?P<x2>\d+) \s+ (?P<y2>\d+) \s*$',  re.VERBOSE)
-FILL_RE = re.compile(r'^ B \s+ (?P<x>\d+) \s+ (?P<y>\d+) \s+ (?P<color>\w) \s*$',  re.VERBOSE)
+VERTICAL_BORDER = '|'
+HORIZONTAL_BORDER = '-'
+LINE_POINT = 'x'
 
-class ParsingError(Exception): pass
-class CanvasParsingError(ParsingError): pass
-class WrongLineTypeError(Exception): pass
-class OutsideCanvasError(Exception): pass
+CANVAS_RE = re.compile(r'''^C \s+
+ (?P<width>\d+) \s+
+ (?P<height>\d+) \s*$''', re.VERBOSE)
+LINE_RE = re.compile(r'''^L \s+
+ (?P<x1>\d+) \s+
+ (?P<y1>\d+) \s+
+ (?P<x2>\d+) \s+
+ (?P<y2>\d+) \s*$''', re.VERBOSE)
+RECTANGLE_RE = re.compile(r'''^R \s+
+ (?P<x1>\d+) \s+
+ (?P<y1>\d+) \s+
+ (?P<x2>\d+) \s+
+ (?P<y2>\d+) \s*$''', re.VERBOSE)
+FILL_RE = re.compile(r'''^B \s+
+ (?P<x>\d+) \s+
+ (?P<y>\d+) \s+
+ (?P<color>\w) \s*$''', re.VERBOSE)
+
+
+class ParsingError(Exception): pass  # noqa
+class CanvasParsingError(ParsingError): pass  # noqa
+class WrongLineTypeError(Exception): pass  # noqa
+class OutsideCanvasError(Exception): pass  # noqa
 
 
 def parse_canvas_cmd(line):
@@ -40,10 +57,10 @@ def create_canvas(width, heigth):
 
 def draw_canvas_border(canvas, vertical_char, horizontal_char):
     max_x, max_y = len(canvas)-1, len(canvas[0])-1
-    draw_line(canvas, 0, 1, 0, max_y-1, vertical_char, True) # left vert
-    draw_line(canvas, max_x, 1, max_x, max_y-1, vertical_char, True) # right vert
-    draw_line(canvas, 0, 0, max_x, 0, horizontal_char, True) # top horiz
-    draw_line(canvas, 0, max_y, max_x, max_y, horizontal_char, True) # bottom horiz
+    line(canvas, 0, 1, 0, max_y-1, vertical_char)  # left vert
+    line(canvas, max_x, 1, max_x, max_y-1, vertical_char)  # right vert
+    line(canvas, 0, 0, max_x, 0, horizontal_char)  # top horiz
+    line(canvas, 0, max_y, max_x, max_y, horizontal_char)  # bottom horiz
 
 
 def show(canvas):
@@ -53,49 +70,67 @@ def show(canvas):
         print()
 
 
-def dot_inside_canvas(canvas, x, y):
+def point_inside_canvas(canvas, x, y):
     max_x, max_y = len(canvas)-2, len(canvas[0])-2
     if 1 <= x <= max_x and 1 <= y <= max_y:
         return True
     else:
         return False
 
+
+# Can be extended to any points count if will needed
+def points_inside_canvas(canvas, x1, y1, x2, y2):
+    point1_inside = point_inside_canvas(canvas, x1, y1)
+    point2_inside = point_inside_canvas(canvas, x2, y2)
+    if point1_inside and point2_inside:
+        return True
+    return False
+
+
 def parse_line_cmd(line):
     res = LINE_RE.match(line)
     if res:
-        return int(res.group('x1')), int(res.group('y1')), int(res.group('x2')), int(res.group('y2'))
+        x1, y1 = int(res.group('x1')), int(res.group('y1'))
+        x2, y2 = int(res.group('x2')), int(res.group('y2'))
+        return x1, y1, x2, y2
     else:
         raise ParsingError(line)
 
 
-def draw_line(canvas, x1, y1, x2, y2, dot_char, is_border=False):
-    if not is_border:
-        if not (dot_inside_canvas(canvas, x1, y1) and dot_inside_canvas(canvas, x2, y2)):
-            raise OutsideCanvasError('{0} {1} {2} {3}'.format(x1, y1, x2, y2))
-    if x1 == x2: # vertical line
-        for y in range(y1,y2+1):
-            canvas[x1][y] = dot_char
-    elif y1 == y2: # horizontal line
-        for x in range(x1,x2+1):
-            canvas[x][y1] = dot_char
+def line(canvas, x1, y1, x2, y2, point_char):
+    if x1 == x2:  # vertical line
+        for y in range(y1, y2+1):
+            canvas[x1][y] = point_char
+    elif y1 == y2:  # horizontal line
+        for x in range(x1, x2+1):
+            canvas[x][y1] = point_char
     else:
         raise WrongLineTypeError('{0} {1} {2} {3}'.format(x1, y1, x2, y2))
+
+
+def draw_line(canvas, x1, y1, x2, y2, point_char):
+    if not points_inside_canvas(canvas, x1, y1, x2, y2):
+        raise OutsideCanvasError('{0} {1} {2} {3}'.format(x1, y1, x2, y2))
+    line(canvas, x1, y1, x2, y2, point_char)
 
 
 def parse_rectangle_cmd(line):
     res = RECTANGLE_RE.match(line)
     if res:
-        return int(res.group('x1')), int(res.group('y1')), int(res.group('x2')), int(res.group('y2'))
+        x1, y1 = int(res.group('x1')), int(res.group('y1'))
+        x2, y2 = int(res.group('x2')), int(res.group('y2'))
+        return x1, y1, x2, y2
     else:
         raise ParsingError(line)
 
-def draw_rectangle(canvas, x1, y1, x2, y2, dot_char):
-    if not (dot_inside_canvas(canvas, x1, y1) and dot_inside_canvas(canvas, x2, y2)):
+
+def draw_rectangle(canvas, x1, y1, x2, y2, point_char):
+    if not points_inside_canvas(canvas, x1, y1, x2, y2):
         raise OutsideCanvasError('{0} {1} {2} {3}'.format(x1, y1, x2, y2))
-    draw_line(canvas, x1, y1, x1, y2, dot_char) # left vert
-    draw_line(canvas, x2, y1, x2, y2, dot_char) # right vert
-    draw_line(canvas, x1, y1, x2, y1, dot_char) # top horiz
-    draw_line(canvas, x1, y2, x2, y2, dot_char) # bottom horiz
+    draw_line(canvas, x1, y1, x1, y2, point_char)  # left vert
+    draw_line(canvas, x2, y1, x2, y2, point_char)  # right vert
+    draw_line(canvas, x1, y1, x2, y1, point_char)  # top horiz
+    draw_line(canvas, x1, y2, x2, y2, point_char)  # bottom horiz
 
 
 def parse_fill_cmd(line):
@@ -107,10 +142,9 @@ def parse_fill_cmd(line):
 
 
 def fill(canvas, x, y, color):
-    max_x, max_y = len(canvas)-1, len(canvas[0])-1
-    if not dot_inside_canvas(canvas, x, y):
+    if not point_inside_canvas(canvas, x, y):
         return
-    if canvas[x][y] in [CANVAS_VERTICAL_BORDER, CANVAS_HORIZONTAL_BORDER, LINE_DOT]:
+    if canvas[x][y] in [VERTICAL_BORDER, HORIZONTAL_BORDER, LINE_POINT]:
         return
     if canvas[x][y] == color:
         return
@@ -126,15 +160,15 @@ def draw(file_name):
         line = file.readline()
         width, heigth = parse_canvas_cmd(line)
         canvas = create_canvas(width, heigth)
-        draw_canvas_border(canvas, CANVAS_VERTICAL_BORDER, CANVAS_HORIZONTAL_BORDER)
-        show(canvas)
+        draw_canvas_border(canvas, VERTICAL_BORDER, HORIZONTAL_BORDER)
+        yield canvas
         for line in file:
             if line[0] == 'L':
                 x1, y1, x2, y2 = parse_line_cmd(line)
-                draw_line(canvas, x1, y1, x2, y2, LINE_DOT)
+                draw_line(canvas, x1, y1, x2, y2, LINE_POINT)
             elif line[0] == 'R':
                 x1, y1, x2, y2 = parse_rectangle_cmd(line)
-                draw_rectangle(canvas, x1, y1, x2, y2, LINE_DOT)
+                draw_rectangle(canvas, x1, y1, x2, y2, LINE_POINT)
             elif line[0] == 'B':
                 x, y, color = parse_fill_cmd(line)
                 fill(canvas, x, y, color)
@@ -142,13 +176,14 @@ def draw(file_name):
                 continue
             else:
                 raise ParsingError(line)
-            show(canvas)
+            yield canvas
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
     try:
-        draw(arguments['<commands_file_name>'])
+        for canvas in draw(arguments['<commands_file_name>']):
+            show(canvas)
     except CanvasParsingError as e:
         print('First line must be a valid canvas drawing command: ', e)
     except ParsingError as e:
